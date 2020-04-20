c90dfc32dab0e2f9140ac5249dcf16262f455d09
Djamilson <djamilson@gmail.com>
2020-04-20 14:22:33 -0300

    Deploy 7

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -4,13 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.microsoft.codepush.react.CodePush;
-import com.oblador.vectoricons.VectorIconsPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
